f622733dbed1c6df2e1306480ddaf54d5124ffbe
TimaDawaa <tima302t@gmail.com>
2024-04-20 22:42:46 +0300

    Create Reservation Model and Cubit

---
+++ travelapp_flutter/lib/features/flight_booking/data/repos/flight_booking_impl_repo.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:travelapp_flutter/core/helpers/api_service.dart';
+import 'package:travelapp_flutter/core/helpers/failure.dart';
+import 'package:travelapp_flutter/features/flight_booking/data/repos/flight_booking_repo.dart';
+
+class FlightBookingImp extends FlightBookingRepo {
+  final ApiService apiService;
+  String? token;
+
+  FlightBookingImp({required this.apiService});
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> getFlightReservationData(
+      {required String id}) async {
+    try {
+      Map<String, dynamic> response = await apiService.get(
+        endPoint: "/flights/reservation/6623902d1ead5ad33f574672",
+        headers: {
+          "Authorization": 'Bearer $token',
+        },
+      );
+      return right(response);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(errMessage: 'Something went wrong'));
+      }
+    }
+  }
+}
