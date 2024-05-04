266cc829d0fd0a1aeba6e0cb20accb75e4dc4d27
TimaDawaa <tima302t@gmail.com>
2024-05-04 16:16:39 +0300

    Add Payment Feature

---
+++ travelapp_flutter/lib/core/widgets/paypal_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class PayPal extends StatelessWidget {
+  const PayPal({super.key, required this.url, required this.widget});
+  final String url;
+  final Widget widget;
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: url,
+      javascriptMode: JavascriptMode.unrestricted,
+      gestureRecognizers: Set()
+        ..add(Factory<DragGestureRecognizer>(
+            () => VerticalDragGestureRecognizer())),
+      onPageFinished: (value) {
+        debugPrint(value);
+      },
+      navigationDelegate: (NavigationRequest request) async {
+        if (request.url.contains("http://return_url/?status=success")) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: ((context) =>  widget),
+            ),
+          );
+        }
+        if (request.url.contains(
+            "https://9a3b-89-39-107-193.ngrok-free.app/payment/cancel")) {
+         // debugPrint("failure");
+          // Get.back();
+        }
+        return NavigationDecision.navigate;
+      },
+    );
+  }
+}
+
+
+// import 'package:flutter/material.dart';
+// import 'package:travelapp_flutter/core/helpers/paypal_widget.dart';
+
+// class Test extends StatelessWidget {
+//   const Test({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         body: Center(
+//           child: TextButton(
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: ((context) => const Center(
+//                         child: PayPal(),
+//                       )),
+//                 ),
+//               );
+//             },
+//             child: const Text("PayPal"),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- travelapp_flutter/lib/main.dart
+++ travelapp_flutter/lib/main.dart
@@ -5,17 +5,12 @@ import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travelapp_flutter/core/helpers/service_locator.dart';
 import 'package:travelapp_flutter/core/utils/constants.dart';
-import 'package:travelapp_flutter/core/utils/themes.dart';
 import 'package:travelapp_flutter/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:travelapp_flutter/features/auth/presentation/view_model/login_cubit/login_cubit.dart';
 import 'package:travelapp_flutter/features/auth/presentation/view_model/profile_cubit/profile_cubit.dart';
 import 'package:travelapp_flutter/features/auth/presentation/view_model/register_cubit/register_cubit.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:travelapp_flutter/features/auth/presentation/views/fetch_profile_data_page.dart';
-import 'package:travelapp_flutter/features/auth/presentation/views/login_page.dart';
-import 'package:travelapp_flutter/features/flight_booking/presentation/views/all_flights_page.dart';
 import 'package:travelapp_flutter/features/flight_booking/presentation/views/confirm_flight_reservation_page.dart';
-import 'package:travelapp_flutter/features/flight_booking/presentation/views/plane_page.dart';
 import 'package:travelapp_flutter/firebase_options.dart';
 
 void main() async {
