8db4953db77c19046ede9affe14955fec0ffaed6
sara najati <saranajatilaptop@gmail.com>
2024-08-17 10:41:37 +0300

    buttons

--- travelapp_flutter/lib/features/home/presentation/views/widgets/home_page_widgets/reserve_buttons.dart
+++ travelapp_flutter/lib/features/home/presentation/views/widgets/home_page_widgets/reserve_buttons.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
@@ -12,6 +13,7 @@ import 'package:travelapp_flutter/core/utils/assets.dart';
 import 'package:travelapp_flutter/core/utils/styles.dart';
 import 'package:travelapp_flutter/core/utils/themes.dart';
 import 'package:travelapp_flutter/features/organizing_trip/presentation/views/organizing_trip_page.dart';
+import 'package:travelapp_flutter/features/settings/presentation/view_model/profile_cubit/profile_cubit.dart';
 
 class ReserveButtons extends StatefulWidget {
   const ReserveButtons({super.key});
@@ -21,6 +23,14 @@ class ReserveButtons extends StatefulWidget {
 }
 
 class _ReserveButtonsState extends State<ReserveButtons> {
+  bool? isOrganizer;
+
+  @override
+  void initState() {
+    super.initState();
+    isOrganizer = BlocProvider.of<ProfilePageCubit>(context).organizer;
+  }
+
   @override
   Widget build(BuildContext context) {
     ValueNotifier<bool> isDialOpen = ValueNotifier(false);
@@ -61,89 +71,115 @@ class _ReserveButtonsState extends State<ReserveButtons> {
                   ),
                 ),
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.32,
-                height: 110,
-                child: SpeedDial(
-                  elevation: 0,
-                  overlayOpacity: 0.6,
-                  openCloseDial: isDialOpen,
-                  childrenButtonSize: const Size(100, 100),
-                  backgroundColor: Colors.transparent,
-                  direction: SpeedDialDirection.down,
-                  shape: const CircleBorder(),
-                  children: [
-                    SpeedDialChild(
-                      elevation: 8,
-                      shape: const CircleBorder(),
-                      backgroundColor: Colors.grey[300],
-                      labelWidget: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Get.to(() =>
-                                  const OrganizingTripPage(groupTrip: false));
-                              isDialOpen.value = false;
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.all(18),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.grey[300],
-                              ),
-                              child: Column(
-                                children: [
-                                  Icon(FontAwesomeIcons.person,
-                                      color: Themes.primary),
-                                  const Text('Single'),
-                                ],
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 20),
-                          GestureDetector(
-                            onTap: () {
-                              Get.to(() =>
-                                  const OrganizingTripPage(groupTrip: true));
-                              isDialOpen.value = false;
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.all(18),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.grey[300],
-                              ),
-                              child: Column(
-                                children: [
-                                  Icon(FontAwesomeIcons.peopleGroup,
-                                      color: Themes.primary),
-                                  const Text('Group'),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                  child: Card(
-                    surfaceTintColor: Colors.white,
-                    color: Themes.primary.withOpacity(0.6),
-                    elevation: 3,
+              if (isOrganizer == true)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.32,
+                  height: 110,
+                  child: SpeedDial(
+                    elevation: 0,
+                    overlayOpacity: 0.6,
+                    openCloseDial: isDialOpen,
+                    childrenButtonSize: const Size(100, 100),
+                    backgroundColor: Colors.transparent,
+                    direction: SpeedDialDirection.down,
                     shape: const CircleBorder(),
-                    shadowColor: Colors.grey,
-                    child: Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: Lottie.asset(
-                        Assets.trip,
-                        alignment: Alignment.center,
-                        fit: BoxFit.contain,
+                    children: [
+                      SpeedDialChild(
+                        elevation: 8,
+                        shape: const CircleBorder(),
+                        backgroundColor: Colors.grey[300],
+                        labelWidget: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Get.to(() =>
+                                    const OrganizingTripPage(groupTrip: false));
+                                isDialOpen.value = false;
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(18),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.grey[300],
+                                ),
+                                child: Column(
+                                  children: [
+                                    Icon(FontAwesomeIcons.person,
+                                        color: Themes.primary),
+                                    const Text('Single'),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            GestureDetector(
+                              onTap: () {
+                                Get.to(() =>
+                                    const OrganizingTripPage(groupTrip: true));
+                                isDialOpen.value = false;
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(18),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.grey[300],
+                                ),
+                                child: Column(
+                                  children: [
+                                    Icon(FontAwesomeIcons.peopleGroup,
+                                        color: Themes.primary),
+                                    const Text('Group'),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                    child: Card(
+                      surfaceTintColor: Colors.white,
+                      color: Themes.primary.withOpacity(0.6),
+                      elevation: 3,
+                      shape: const CircleBorder(),
+                      shadowColor: Colors.grey,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Lottie.asset(
+                          Assets.trip,
+                          alignment: Alignment.center,
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
+              if (isOrganizer == false)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.32,
+                  height: 110,
+                  child: GestureDetector(
+                    onTap: () {
+                      Get.to(() => const OrganizingTripPage(groupTrip: false));
+                    },
+                    child: Card(
+                      surfaceTintColor: Colors.white,
+                      color: Themes.primary.withOpacity(0.6),
+                      elevation: 3,
+                      shape: const CircleBorder(),
+                      shadowColor: Colors.grey,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Lottie.asset(
+                          Assets.trip,
+                          alignment: Alignment.center,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.28,
                 height: 80,
